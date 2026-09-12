86168452aa03c7291a9dfb6111b56ab9d744fcb3
Eduarda Bueno de Salvo <dudasalvo@gmail.com>
2026-09-11 23:26:18 -0300

    feat: adicionando exercicio 01

---
+++ exercicio01/lib/main.dart
@@ -0,0 +1,66 @@
+// exercicio 01 - Lista de Linguagens
+// Crie uma List<String> com pelo menos oito linguagens e apresente-as com ListView.builder e ListTile
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TelaLinguagens(),
+    );
+  }
+}
+
+class TelaLinguagens extends StatelessWidget {
+  const TelaLinguagens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> linguagens = [
+      'Flutter',
+      'Python',
+      'Java',
+      'TypeScript',
+      'C++',
+      'Assembly',
+      'R',
+      'SQL',
+      'Dart',
+      'C',
+    ];
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Lista de Linguagens')),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(18),
+        itemCount: linguagens.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              leading: const Icon(
+                Icons.code,
+                size: 35,
+                color: Color.fromRGBO(0, 103, 79, 1.0), // opacidade de 0.0 a 1.0
+              ),
+              title: Text(
+                linguagens[index],
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+// Usamos const em Widgets com valores fixos para criá-los na compilação e economizar memória.
+// Widgets dinâmicos — como Text(linguagens[index]) — mudam em execução e não podem receber const.
